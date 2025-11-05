90e5bb146e330fd53ef2e1b5acdf75efbf87fddf
Jango <101262528+Jangveersoothe@users.noreply.github.com>
2025-11-05 09:40:18 +0530

    Initialized ServiceHive Assignment

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("SlotSwapper backend running");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on localhost: ${PORT}`);
+});
